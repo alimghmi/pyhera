c48c5f8fc05c5112fe8977881ef6c9b0934683c8
Ali Moghimi <ThisisJavid@Gmail.com>
2020-04-12 19:31:01 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-
+from distutils.core import setup
+setup(
+  name = 'pyhera',
+  packages = ['pyhera'],
+  version = '0.1',
+  license='MIT', 
+  description = 'Hera is an optimized in-memory database', 
+  author = 'Ali Moghimi', 
+  author_email = 'thisisjavid@gmail.com',
+  url = 'https://github.com/lastill/Hera',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz', 
+  keywords = ['dbms', 'database', 'nosql', 'hera', 'heradb', 'pyhera'], 
+  classifiers=[
+    'Development Status :: 4 - Beta',      # "3 - Alpha" | "4 - Beta" | "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License'
+  ],
+)
