8ffe7a1d88dbc3f7e5a3f38fba92b20250cfd74a
Ali Moghimi <ThisisJavid@Gmail.com>
2020-04-12 20:51:21 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'pyhera',
   packages = ['pyhera'],
-  version = '0.1',
-  license='MIT', 
-  description = 'Hera is an optimized in-memory database', 
-  author = 'Ali Moghimi', 
+  version = '0.5',
+  license='MIT',
+  description = 'Hera is an optimized in-memory database',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Ali Moghimi',
   author_email = 'thisisjavid@gmail.com',
-  url = 'https://github.com/lastill/Hera', 
-  keywords = ['dbms', 'database', 'nosql', 'hera', 'heradb', 'pyhera'], 
+  url = 'https://github.com/lastill/pyhera',
+  keywords = ['dbms', 'database', 'nosql', 'hera', 'heradb', 'pyhera'],
   classifiers=[
     'Development Status :: 4 - Beta',      # "3 - Alpha" | "4 - Beta" | "5 - Production/Stable"
     'Intended Audience :: Developers',
